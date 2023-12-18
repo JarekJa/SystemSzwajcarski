037fddca4938201d3bf4f61ea7a8de9c8d563bd6
Jarek Jabłoński <jarek.ja1999.pl@gmail.com>
2023-12-18 15:59:31 +0100

    Dodaj pliki projektów.

---
+++ SystemSzwajcarski/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SystemSzwajcarski.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
